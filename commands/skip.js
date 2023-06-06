3d5ab0b67df89a5fca50c2fe2f29ba51ce1b1237
skmz99 <83142392+skmz99@users.noreply.github.com>
2023-06-06 16:26:49 -0500

    removed command "skip index" but works with new updates

--- commands/skip.js
+++ commands/skip.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
+const { EmbedBuilder, Embed } = require("discord.js")
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,20 +9,20 @@ module.exports = {
             subcommand
             .setName("current")
             .setDescription("Skip Current Song")
-        )
-        .addSubcommand(subcommand =>
-			subcommand
-				.setName("index")
-				.setDescription("Skip Song From Index")
-				.addStringOption(option =>
-					option.setName("index").setDescription("Skip Song From Index").setRequired(true)
-				)
-		),
+        ),
+        // .addSubcommand(subcommand =>
+		// 	subcommand
+		// 		.setName("index")
+		// 		.setDescription("Skip Song From Index")
+		// 		.addStringOption(option =>
+		// 			option.setName("index").setDescription("Skip Song From Index").setRequired(true)
+		// 		)
+		// ),
 
 	execute: async ({ client, interaction }) => {
 
         // Get the queue for the server
-		const queue = await client.player.getQueue(interaction.guildId)
+		const queue = await client.player.nodes.get(interaction.guildId)
 
         // If there is no queue, return
 		if (!queue)
@@ -33,57 +33,20 @@ module.exports = {
 
         if (interaction.options.getSubcommand() == 'current'){
 
-            const currentSong = queue.current
+            const currentSong = queue.currentTrack
             
             // Skip the current song
-            queue.skip()
+            queue.node.skip()
             
             // Return an embed to the user saying the song has been skipped
             await interaction.reply({
                 embeds: [
-                    new MessageEmbed()
+                    new EmbedBuilder()
                     .setDescription(`${currentSong.title} has been skipped!`)
                     .setThumbnail(currentSong.thumbnail)
                 ]
             })
         }
-        else if(interaction.options.getSubcommand() == 'index'){
-            const queueSize = queue.tracks.length
-            let index = interaction.options.getString("index")
-            if (queueSize < index || index < 0){
-                await interaction.reply({
-                    embeds: [
-                        new MessageEmbed()
-                        .setDescription(`Index ${index} is out of bounds. Size of Queue is ${queueSize}`)
-                    ]
-                })
-            }
-            else if(index == 0){
-                const currentSong = queue.current
-            
-                // Skip the current song
-                queue.skip()
-                
-                // Return an embed to the user saying the song has been skipped
-                await interaction.reply({
-                    embeds: [
-                        new MessageEmbed()
-                        .setDescription(`**${currentSong.title}** has been skipped!`)
-                        .setThumbnail(currentSong.thumbnail)
-                    ]
-                })
-            }
-            else{
-                queue[i].skip()
-                await interaction.reply({
-                    embeds: [
-                        new MessageEmbed()
-                        .setDescription(`**${queue[i]}** has been skipped!`)
-                        .setThumbnail(queue[i].thumbnail)
-                    ]
-                })
-            }
-            return
-        }
+        return
 	},
 }
